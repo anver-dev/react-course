f4b04d68b4ee4665c7720f97e866d51a03ad4d98
Ana Karina Vergara Guzmán <karinaagp26@gmail.com>
2021-11-17 01:14:29 -0600

    Variables
    Repaso de variables en JS.

---
+++ 02/intro-js/src/bases/variables.js
@@ -0,0 +1,14 @@
+// Variables y constantes
+
+const nombre = "Karina";
+const apellido = "Guzmán";
+
+let valorDado = 5;
+
+console.log({ nombre, apellido, valorDado });
+
+if (true) {
+    let valorDado = 6;
+
+    console.log(valorDado)
+}
