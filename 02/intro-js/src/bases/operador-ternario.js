83b86b85f4bee3e60797362591af885897c8ece3
Ana Karina Vergara Guzmán <karinaagp26@gmail.com>
2021-11-20 10:28:00 -0600

    Operador ternario

---
+++ 02/intro-js/src/bases/operador-ternario.js
@@ -0,0 +1,7 @@
+const activo = true;
+
+// operador ternario
+let mensaje = activo ? "activo" : "inactivo";
+
+// si no se requiere el 'else' del operador ternario 
+let mensajeCorto = activo && "activo";
