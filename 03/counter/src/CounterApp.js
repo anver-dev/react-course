23e4e398a7be08bfa14ca0ce5797b73e3833713c
Ana Karina Vergara Guzmán <karinaagp26@gmail.com>
2021-11-21 14:32:03 -0600

    useState - hook

--- 03/counter/src/CounterApp.js
+++ 03/counter/src/CounterApp.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const CounterApp = ({value}) => {
+const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(0); // [valor del estado, funcion que lo modifica]
+  const handleAdd = (e) => {
+    setCounter(counter + 1);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> {value} </h2>
+      <h2> {counter} </h2>
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 };
 
 CounterApp.propTypes = {
-    value: PropTypes.number.isRequired
-}
+  value: PropTypes.number.isRequired,
+};
 
 export default CounterApp;
