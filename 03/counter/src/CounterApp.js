bed96475d76e710fa3f41ba5b93046b379dd3394
Ana Karina Vergara Guzmán <karinaagp26@gmail.com>
2021-11-21 14:42:25 -0600

    Ejemplos del useState

--- 03/counter/src/CounterApp.js
+++ 03/counter/src/CounterApp.js
@@ -2,9 +2,18 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
-  const [counter, setCounter] = useState(0); // [valor del estado, funcion que lo modifica]
+  const [counter, setCounter] = useState(value); // [valor del estado, funcion que lo modifica]
+
   const handleAdd = (e) => {
     setCounter(counter + 1);
+  };
+
+  const handleReset = (e) => {
+    setCounter(value);
+  };
+
+  const handleSubstract = (e) => {
+    setCounter(counter - 1);
   };
 
   return (
@@ -12,6 +21,8 @@ const CounterApp = ({ value }) => {
       <h1>CounterApp</h1>
       <h2> {counter} </h2>
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleSubstract}>-1</button>
     </>
   );
 };
